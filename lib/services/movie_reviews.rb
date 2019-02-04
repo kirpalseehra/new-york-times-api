61018ef396696066c645200bff0e095e666eaa2c
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-04 11:03:03 +0000

    created a loop so I am able to retrieve all the titles in the results

--- lib/services/movie_reviews.rb
+++ lib/services/movie_reviews.rb
@@ -12,12 +12,41 @@ class MovieReview
     @movie_review_request = JSON.parse(self.class.get("/svc/movies/v2/reviews/search.json?query=gladiator&api-key=tVjawWx5MnoSNJuisipioq59ZIE4qWnw").body)
   end
 
-  def retrieve_review_results 
-    @movie_review_request['status']
+  def retrieve_review
+    @movie_review_request
   end
 
+  def retrieve_review_status
+   retrieve_review['status']
+  end
+
+  def retrieve_review_copyright
+    retrieve_review['copyright']
+  end
+
+  def retrieve_review_has_more
+    retrieve_review['has_more']
+  end
+
+  def retrieve_review_num_results
+    retrieve_review['num_results']
+  end
+
+  def retrieve_review_results
+    retrieve_review['results']
+  end
+
+  def retrieve_all_titles
+    title_loop = []
+    retrieve_review_results.each do |title|
+      title_loop.push title['display_title']
+    end
+    title_loop
+  end
+
+  
 end
 
 test = MovieReview.new
-test.current_movie_reviews('tVjawWx5MnoSNJuisipioq59ZIE4qWnw')
-p test.retrieve_review_results
+test.current_movie_reviews('')
+p test.retrieve_all_titles
